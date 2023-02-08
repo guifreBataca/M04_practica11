5686e552af045468da09dd7f56ae2ebbb15e6495
lMrSl <apalenqu22@jaumebalmes.net>
2023-02-08 20:20:34 +0100

    main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from guifre import exercici1_A as ex1A
 from guifre import exercici2_A as ex2A
 from guifre import exercic3_A as ex3A
+from Sebastian import exercici1_B as ex1B
+from Sebastian import exercici2_B as ex2B
+from Sebastian import exercici3_B as ex3B
 
 ex1A.mesGran()
 ex2A.salutacions()
 ex3A.numeroSort()
+
+ex1B.ejer1()
+ex2B.ejer2()
+ex3B.ejer3()
